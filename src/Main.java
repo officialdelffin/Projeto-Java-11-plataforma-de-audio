d79172f7b431c684decaf31a9501ef1a3af931a6
officialdelffin <workdelffin@gmail.com>
2026-01-28 20:41:15 -0300

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/* Projeto :  Plataforma de Áudio
+
+Simulador de áudio via terminal para consolidar POO em Java. Composto pela classe mãe Audio e subclasses Musica e Podcast. Utiliza uma interface com o metodo tocarSom e uma classe de serviço para execução,
+aplicando conceitos de herança e polimorfismo para tratar objetos distintos de forma genérica e desacoplada. */
+
+
+// Classe principal do sistema :
+public class Main {
+
+
+    // Funcção que faz a execução do sistema :
+    public static void main( String[] args ) {
+
+
+
+
+
+    }
+
+
+}
