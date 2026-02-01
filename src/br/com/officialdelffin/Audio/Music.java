cb50bd833bac0262d99086a7cf49c8e6cdd57831
officialdelffin <workdelffin@gmail.com>
2026-02-01 13:16:08 -0300

    commit 5

--- src/br/com/officialdelffin/Audio/Music.java
+++ src/br/com/officialdelffin/Audio/Music.java
@@ -3,10 +3,23 @@
 package br.com.officialdelffin.Audio;
 
 
+import br.com.officialdelffin.Interfaces.Playable;
+
 // Classe filha musica :
-public class Music extends Audio {
+public class Music extends Audio implements Playable {
 
 
+
+    @Override
+    public String getPlaying (){
+
+
+        String audioPlaybackDisplay = "A musica está sendo reproduzida";
+
+        return audioPlaybackDisplay;
+
+
+    }
 
 
 
--- src/br/com/officialdelffin/Services/PlayerAudio.java
+++ src/br/com/officialdelffin/Services/PlayerAudio.java
@@ -15,6 +15,8 @@ public class PlayerAudio {
     public void playingMidia ( Playable playingAudio ) {
 
 
+
+
         playingAudio.getPlaying();
 
 
